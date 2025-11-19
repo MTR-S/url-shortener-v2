7e5a4cc464d443ff19ef06661c70f97081f0fdab
MTR-S <matheusalmeidass14@gmail.com>
2025-11-19 20:00:18 -0300

    feat: controller class to expose the logic the endpoints

---
+++ urlShortener/src/main/java/com/myProject/urlShortener/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.myProject.urlShortener.controller;
+
+import com.myProject.urlShortener.data.dto.ShortenUrlRequest;
+import com.myProject.urlShortener.data.dto.ShortenUrlResponse;
+import com.myProject.urlShortener.service.UrlService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MainController {
+
+    private final UrlService urlService;
+
+    public MainController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<ShortenUrlResponse> shorten(@RequestBody ShortenUrlRequest request, HttpServletRequest servletRequest) {
+        String shortCode = urlService.shortMyUrl(request.originalUrl());
+
+        String redirectUrl = servletRequest.getRequestURL().toString().replace("/shorten", "/" + shortCode);
+
+        ShortenUrlResponse response = new ShortenUrlResponse(redirectUrl, shortCode);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+        String originalUrl = urlService.getOriginalUrl(shortCode);
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, originalUrl)
+                .build();
+    }
+}
