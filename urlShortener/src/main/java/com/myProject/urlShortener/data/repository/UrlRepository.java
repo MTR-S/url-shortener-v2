4618d73aefea9ea20b897fff585db85c8e4dc4da
MTR-S <matheusalmeidass14@gmail.com>
2025-11-19 19:59:36 -0300

    feat: Url repository to interact  with database, methods: save and findByShortCode

---
+++ urlShortener/src/main/java/com/myProject/urlShortener/data/repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.myProject.urlShortener.data.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository {
+    void save(String shortCode, String originalUrl);
+    Optional<String> findByShortCode(String shortCode);
+}
