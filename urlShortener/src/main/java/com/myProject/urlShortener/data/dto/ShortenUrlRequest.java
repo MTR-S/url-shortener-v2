56028872aedcd0d2deb4742ad019891ab8645908
MTR-S <matheusalmeidass14@gmail.com>
2025-11-19 19:58:50 -0300

    feat: dto's for main endpoints (URL request and response)

---
+++ urlShortener/src/main/java/com/myProject/urlShortener/data/dto/ShortenUrlRequest.java
@@ -0,0 +1,4 @@
+package com.myProject.urlShortener.data.dto;
+
+public record ShortenUrlRequest(String originalUrl) {
+}
---
+++ urlShortener/src/main/java/com/myProject/urlShortener/data/dto/ShortenUrlResponse.java
@@ -0,0 +1,4 @@
+package com.myProject.urlShortener.data.dto;
+
+public record ShortenUrlResponse(String redirectUrl, String shortCode) {
+}
