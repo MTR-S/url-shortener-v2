db52fb965a8bbb73a0c402e34cb637a2d9b66253
MTR-S <matheusalmeidass14@gmail.com>
2025-11-19 19:05:54 -0300

    feat: encode and decode method implemented for test

--- urlShortener/src/main/java/com/myProject/urlShortener/service/UrlService.java
+++ urlShortener/src/main/java/com/myProject/urlShortener/service/UrlService.java
@@ -1,13 +1,26 @@
 package com.myProject.urlShortener.service;
 
+import org.springframework.stereotype.Service;
+import com.myProject.urlShortener.service.interfaces.Encoder;
+import com.myProject.urlShortener.service.interfaces.Decoder;
+
+@Service
 public class UrlService{
-    @Override
-    public String encode(Integer autoIncrementId) {
-        return "";
+
+    Encoder encoderInterface;
+    Decoder decoderInterface;
+
+    public UrlService(Encoder encoderInterface, Decoder decoderInterface) {
+        this.encoderInterface = encoderInterface;
+        this.decoderInterface = decoderInterface;
     }
 
-    @Override
-    public String decode(String stringToBeDecoded) {
-        return "";
+    public String encode(long autoIncrementId) {
+        return encoderInterface.encode(autoIncrementId);
+    }
+
+
+    public long decode(String stringToBeDecoded) {
+        return decoderInterface.decode(stringToBeDecoded);
     }
 }
