9b0755978b25bebaa162d7d60bec13f60f71860c
MTR-S <matheusalmeidass14@gmail.com>
2025-11-25 18:56:26 -0300

    feat: new methods implemented and autowired of url repository

--- urlShortener/src/main/java/com/myProject/urlShortener/service/UrlService.java
+++ urlShortener/src/main/java/com/myProject/urlShortener/service/UrlService.java
@@ -1,8 +1,11 @@
 package com.myProject.urlShortener.service;
 
+import com.myProject.urlShortener.data.repository.UrlRepository;
 import org.springframework.stereotype.Service;
 import com.myProject.urlShortener.service.interfaces.Encoder;
 import com.myProject.urlShortener.service.interfaces.Decoder;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class UrlService{
@@ -10,17 +13,28 @@ public class UrlService{
     Encoder encoderInterface;
     Decoder decoderInterface;
 
-    public UrlService(Encoder encoderInterface, Decoder decoderInterface) {
+    UrlRepository urlRepository;
+
+    public UrlService(Encoder encoderInterface, Decoder decoderInterface,
+                      UrlRepository urlRepository) {
         this.encoderInterface = encoderInterface;
         this.decoderInterface = decoderInterface;
+        this.urlRepository = urlRepository;
     }
 
-    public String encode(long autoIncrementId) {
-        return encoderInterface.encode(autoIncrementId);
+    public String shortMyUrl(String originalUrl) {
+         long id = urlRepository.incrementAndGetId();
+
+        String shortCode = encoderInterface.encode(id);
+
+        urlRepository.save(shortCode, originalUrl);
+
+        return shortCode;
     }
 
 
-    public long decode(String stringToBeDecoded) {
-        return decoderInterface.decode(stringToBeDecoded);
+    public String getOriginalUrl(String shortCode) {
+        return urlRepository.findByShortCode(shortCode)
+                .orElseThrow(() -> new RuntimeException("URL not founded"));
     }
 }
