c054b4a95a443f87e6a4a646a26372f228b88a29
MTR-S <matheusalmeidass14@gmail.com>
2025-11-27 11:34:37 -0300

    feat: new method incrementAndGetOriginalUrl to increment click count and return the original url to redirect

--- urlShortener/src/main/java/com/myProject/urlShortener/data/repository/UrlRepository.java
+++ urlShortener/src/main/java/com/myProject/urlShortener/data/repository/UrlRepository.java
@@ -1,12 +1,14 @@
 package com.myProject.urlShortener.data.repository;
 
+import com.myProject.urlShortener.data.entity.UrlEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UrlRepository {
-    void save(String shortCode, String originalUrl);
+    void save(String shortCode, String originalUrl, Long expirationTime);
     Optional<String> findByShortCode(String shortCode);
-    long incrementAndGetId();
+    long incrementAndGetSequenceId();
+    Optional<UrlEntity> incrementAndGetOriginalUrl(String shortCode);
 }
