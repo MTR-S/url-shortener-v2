1adca304a7e0256c4de9de5f05442254593d40ca
MTR-S <matheusalmeidass14@gmail.com>
2025-12-03 19:58:09 -0300

    feat: new class - RateLimitFilter to be the reverse proxy, filtering POST requisition to the main controller

---
+++ src/main/java/com/myProject/urlShortener/controller/filter/RateLimitFilter.java
@@ -0,0 +1,62 @@
+package com.myProject.urlShortener.controller.filter;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimitFilter implements Filter {
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (!"POST".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(servletRequest, servletResponse);
+
+            return;
+        }
+
+        String ip = getAwsUserRealIp(request);
+
+        // Pega ou cria o balde para esse IP
+        Bucket bucket = cache.computeIfAbsent(ip, this::createNewBucket);
+
+        if (bucket.tryConsume(1)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            response.setStatus(429);
+            response.getWriter().write("{\"error\": \"Too Many Requests. Slow down!\"}");
+        }
+    }
+
+    private Bucket createNewBucket(String key) {
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(10)));
+
+        return Bucket.builder().addLimit(limit).build();
+    }
+
+    private String getAwsUserRealIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.isEmpty()) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
+}
